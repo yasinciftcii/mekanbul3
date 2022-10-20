7f4608cf8ed3b34f5cb9d86a8fbbfa07e0d298e0
Yasin Ciftci <yasinciftci_32@outlook.com>
2022-10-20 22:58:03 +0300

    3. odev degisiklikleri

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -2,16 +2,84 @@ var express = require('express');
 var router = express.Router();
 
 const anaSayfa=function(req, res, next) {
-    res.render('anasayfa', { title: 'Ana sayfa' });
+    res.render('anasayfa',
+    { "baslik": 'Ana Sayfa',
+      "sayfaBaslik":{
+        "siteAd":"Mekanbul",
+        "slogan":"Civardaki Mekanları Keşfet!"
+      },
+      "mekanlar":[
+        {
+          "ad":"Barida Kafe",
+          "adres":"Sdü Batı Kampüsü",
+          "puan":"4",
+          "mesafe":"2km",
+          "imkanlar":["Kahve","Çay","Pasta"]
+        },
+        {
+          "ad":"Gloria Jeans",
+          "adres":"Sdü Doğu Kampüsü",
+          "puan":"2",
+          "mesafe":"5km",
+          "imkanlar":["Kahve","Çay","Pasta"]
+        }
+
+      ]
+  
+  });
   }
 
 const mekanBilgisi=function(req, res, next) {
-    res.render('mekanbilgisi', { title: 'Mekan bilgisi' });
-  }
-const yorumEkle=function(req, res, next) {
-    res.render('yorumekle', { title: 'Yorum ekle' });
-  }
+    res.render('mekanbilgisi',
+    { "baslik": 'Mekan bilgisi',
+      "mekanBaslik":"Starbucks",
+      "mekanDetay":{
+        "ad":"Starbucks",
+        "adres":"Centrum Garden AVM",
+        "puan":"4",
+        "saatler":[
+          {
+            "gunler":"Pazartesi-Cuma",
+            "acilis":"9.00",
+            "kapanis":"23.00",
+            "kapali":"false"
+          },
+          {
+            "gunler":"Cumartesi-Pazar",
+            "acilis":"10.00",
+            "kapanis":"22.00",
+            "kapali":"false"
+          }
+        ],
+        "imkanlar":["Kahve","Çay","Kek"],
+        "koordinatlar":{
+          "enlem":"37.7",
+          "boylam":"30.5"
+        },
+        "yorumlar":[
+          {
+            "yorumYapan":"Yasin",
+            "puan":"3",
+            "tarih":"20 Ekim 2022",
+            "yorumMetni":"Çok berbat"
+          },
+          {
+            "yorumYapan":"Ali",
+            "puan":"5",
+            "tarih":"20 Ekim 2022",
+            "yorumMetni":"Süper"
+          }
+        ]
+
+      }
   
+  });
+  }
+
+const yorumEkle=function(req, res) {
+    res.render('yorumekle', { title: 'Yorum Sayfası' });
+  }
+
   module.exports={
 
     anaSayfa,
